822f2c35b4c7c5a20ede2d234648dc51884db04a
varsha-badda <varshabadda0@gmail.com>
2025-10-04 14:47:31 +0530

    commit

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, createContext } from 'react';
-import { getLocalStorage } from '../utils/LocalStorage';
+import { getLocalStorage, setLocalStorage } from '../utils/LocalStorage';
 
 export const AuthContext = createContext();
 
@@ -7,6 +7,7 @@ const AuthProvider = ({ children }) => {
   const [userData, setUserData] = useState(null);
 
   useEffect(() => {
+    setLocalStorage()
     const { employees, admin } = getLocalStorage();
     setUserData({ employees, admin });
   }, []);
