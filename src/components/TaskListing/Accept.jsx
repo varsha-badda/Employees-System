5c0ebd261be0d3aa3e3431964f5fbd11bdff9068
varsha-badda <varshabadda@gmail.com>
2025-10-09 18:39:47 +0530

    commit

--- src/components/TaskListing/Accept.jsx
+++ src/components/TaskListing/Accept.jsx
@@ -10,8 +10,8 @@ const Accept = () => {
       <h2 className='mt-5 text-2xl font-semibold'>Make a youtube video</h2>
       <p className='text-sm mt-2'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem cum mollitia fuga amet consequatur repudiandae!</p>
       <div className='flex justify-between mt-4'>
-        <button className='bg-green-500 py-1 px-2 text-sm'>mark as completed</button>
-        <button className='bg-red-500 py-1 px-2 text-sm'>mark as failed</button>
+        <button className='bg-green-500 py-1 px-2 text-sm'>Mark as completed</button>
+        <button className='bg-red-500 py-1 px-2 text-sm'>Mark as failed</button>
       </div>
       </div>
   )
