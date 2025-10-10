4bffed977a7f9b1a8cbb00acfef748e09cbf8317
varsha-badda <varshabadda@gmail.com>
2025-10-10 19:08:51 +0530

    commit

--- src/components/others/AllTask.jsx
+++ src/components/others/AllTask.jsx
@@ -1,35 +1,33 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AuthContext } from '../../context/AuthProvider'
 
 
 const AllTask = () => {
-    
+    const authData = useContext(AuthContext)
   return (
-    <div className='bg-[#1c1c1c] p-5 mt-5 rounded h-48 overflow-auto'>
-        <div className='bg-red-400 py-2 px-4 flex justify-between rounded'>
-            <h2>Varsha</h2>
-            <h3>Make a UI design</h3>
-            <h5>Status</h5>
+    <div className='bg-[#1c1c1c] p-5 mt-5 rounded h-52 '>
+      <div className='bg-orange-600 py-2 px-4 flex justify-between rounded'>
+            <h2 className='w-1/5 '>Employee Name</h2>
+            <h3 className='w-1/5 '>New Task</h3>
+            <h5 className='w-1/5 '>Active Task</h5>
+            <h5 className='w-1/5 '>Completed</h5>
+            <h5 className='w-1/5 '>Failed</h5>
         </div>
-        <div className='bg-blue-400 py-2 px-4 flex justify-between rounded'>
-            <h2>Varsha</h2>
-            <h3>Make a UI design</h3>
-            <h5>Status</h5>
+       <div className = 'h-[95%] overflow-auto'>
+         {authData.employees.map(function(elem){
+            return <div className='bg-black py-2 px-4 flex justify-between rounded'>
+            <h2 className='w-1/5 text-white '>{elem.firstName}</h2>
+            <h3 className='w-1/5 text-'>Task</h3>
+            <h5 className='w-1/5 text-white'>Status</h5>
+            <h5 className='w-1/5 text-white'>Status</h5>
+            <h5 className='w-1/5 text-red-700'>Failed</h5>
         </div>
-        <div className='bg-green-400 py-2 px-4 flex justify-between rounded'>
-            <h2>Varsha</h2>
-            <h3>Make a UI design</h3>
-            <h5>Status</h5>
-        </div>
-        <div className='bg-yellow-400 py-2 px-4 flex justify-between rounded'>
-            <h2>Varsha</h2>
-            <h3>Make a UI design</h3>
-            <h5>Status</h5>
-        </div>
-        <div className='bg-purple-400 py-2 px-4 flex justify-between rounded'>
-            <h2>Varsha</h2>
-            <h3>Make a UI design</h3>
-            <h5>Status</h5>
-        </div>
+          })}
+       </div>
+      
+        
+      
+       
       
     </div>
   )
