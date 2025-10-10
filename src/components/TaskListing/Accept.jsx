d9a7f468a2acf14e6764d1e279efd07d5f731089
varsha-badda <varshabadda@gmail.com>
2025-10-10 12:30:20 +0530

    Updates

--- src/components/TaskListing/Accept.jsx
+++ src/components/TaskListing/Accept.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 
-const Accept = () => {
+const Accept = ({data}) => {
+  
   return (
-    <div className='flex-shrink-0 h-full p-5 w-[300px] bg-yellow-400 rounded-xl'>
+    <div className='flex-shrink-0 h-full p-5 w-[370px] bg-yellow-400 rounded-xl'>
           <div className='flex justify-between items-center'>
-          <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>High</h3>
-          <h4 className='text-sm'>20 feb 2025</h4>
+          <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>{data.category}</h3>
+          <h4 className='text-sm'>{data.date}</h4>
       </div>
-      <h2 className='mt-5 text-2xl font-semibold'>Make a youtube video</h2>
-      <p className='text-sm mt-2'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem cum mollitia fuga amet consequatur repudiandae!</p>
+      <h2 className='mt-5 text-2xl font-semibold'>{data.title}</h2>
+      <p className='text-sm mt-2'>{data.description}</p>
       <div className='flex justify-between mt-4'>
         <button className='bg-green-500 py-1 px-2 text-sm'>Mark as completed</button>
         <button className='bg-red-500 py-1 px-2 text-sm'>Mark as failed</button>
--- src/components/others/Header.jsx
+++ src/components/others/Header.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 
 const Header = ({data}) => {
-
+const logOutUser =()=>{
+  localStorage.setItem('loggedInUser','')
+  window.location.reload()
+}
   
   return (
     <div className='flex items-end justify-between'>
       <h1 className='text-2xl font-medium text-white'>HELLO <br/> <span className='text-white text-3xl font-semibold'>{data.firstName} 👋</span></h1>
-      <button className='bg-orange-600 text-white rounded-sm text-lg font-medium px-5 py-2'>Log Out</button>
+      <button onClick={logOutUser} className='bg-orange-600 text-white rounded-sm text-lg font-medium px-5 py-2'>Log Out</button>
     </div>
   )
 }
