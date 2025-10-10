73bcb7ff68750b11217ffcacdbddb18858d82ce8
varsha-badda <varshabadda0@gmail.com>
2025-10-10 19:08:51 +0530

    commit

--- src/components/others/Header.jsx
+++ src/components/others/Header.jsx
@@ -9,7 +9,7 @@ const logOutUser =()=>{
   return (
     <div className='flex items-end justify-between'>
       <h1 className='text-2xl font-medium text-white'>HELLO <br/> <span className='text-white text-3xl font-semibold'>{data.firstName} 👋</span></h1>
-      <button onClick={logOutUser} className='bg-orange-600 text-white rounded-sm text-lg font-medium px-5 py-2'>Log Out</button>
+      <button onClick={logOutUser} className='bg-red-600 text-white rounded-sm text-lg font-medium px-5 py-2'>Log Out</button>
     </div>
   )
 }
